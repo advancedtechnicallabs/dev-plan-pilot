f62f8a5f440b49ef7cf4eb293f9b3177e37a45da
advancedtechnicallabs <87400111+advancedtechnicallabs@users.noreply.github.com>
2024-04-12 18:28:10 +0000

    Adding drag n drop 1

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from 'sortablejs';
+
+// Connects to data-controller="sortable"
+export default class extends Controller {
+  connect() {
+    Sortable.create(this.element, {})
+  }
+  
+}
